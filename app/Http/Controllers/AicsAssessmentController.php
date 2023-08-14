b778e38407b727bda57e3d3fd13628983a34becc
RICTMS <ACER@11ltICTmndeleon.ENTDSWD.LOCAL>
2023-08-14 16:02:37 +0800

    sms response

--- app/Http/Controllers/AicsAssessmentController.php
+++ app/Http/Controllers/AicsAssessmentController.php
@@ -220,7 +220,7 @@ class AicsAssessmentController extends Controller
     {    
         $msg = "Maayong Adlaw! Kani na mensahe gikan sa DSWD Davao Region Office. Pwede na makuha ang  MEDICINE ASSISTANCE. Mamalihog mi na DALAHON ANG ORIGINAL DOCUMENTS na gi upload, apil isa ka VALID ID . Daghang Salamat!";
         $response = Http::get('http://34.80.139.96/api/v2/SendSMS?ApiKey=LWtHZKzgbIh1sNQUPInRyqDFsj8W0K+8YCeSIdN08zA=&ClientId=3b3f49c9-b8e2-4558-9ed2-d618d7743fd5&SenderId=DSWD11AICS&Message='. $msg .'&MobileNumbers=63'.substr($request->aics_client->mobile_number, 1));
-        return $response;
+        return $response->collect();
     }
 
 }
--- app/Http/Controllers/AicsAssistanceController.php
+++ app/Http/Controllers/AicsAssistanceController.php
@@ -375,7 +375,7 @@ class AicsAssistanceController extends Controller
         }
         
         $response = Http::get('http://34.80.139.96/api/v2/SendSMS?ApiKey=LWtHZKzgbIh1sNQUPInRyqDFsj8W0K+8YCeSIdN08zA=&ClientId=3b3f49c9-b8e2-4558-9ed2-d618d7743fd5&SenderId=DSWD11AICS&Message='. $msg .'&MobileNumbers=63'.substr($request->aics_client->mobile_number, 1));
-        return $response;
+        return $response->collect();
     }
 
 }
