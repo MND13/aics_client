6abf4fabd9f62032f87cb3c2aedfb9473de829d7
RICTMS <ACER@11ltICTmndeleon.ENTDSWD.LOCAL>
2023-10-03 09:20:26 +0800

    fix ui reports?

--- resources/js/Components/Reports.vue
+++ resources/js/Components/Reports.vue
@@ -1,29 +1,32 @@
 <template>
     <v-card flat outlined>
-        <v-card-title>Reports <v-progress-circular :width="3" indeterminate color="primary"
-                v-if="isExporting"></v-progress-circular>
+        <v-card-title>Reports 
         </v-card-title>
         <v-card-text>
 
             <v-row>
-                <v-col cols="12" md="2">
-
-
+                <v-col cols="12" md="3">
                     <v-select :items="options" v-model="formData.date_option"></v-select>
+              
 
                     <v-date-picker v-model="formData.date_range" v-if="formData.date_option == 'Custom Range'"
                         range></v-date-picker>
 
+                
 
-                    <v-list dense>
+                    <v-btn dark flat @click="downloadCRIMS" :loading="isExporting">
+                        Export to CRIMS
+                    </v-btn>
+
+                    <!--<v-list dense>
                         <v-list-item link dense>
                             <v-list-item-content>
-                                <v-list-item-title @click="downloadCRIMS" :disabled="isExporting">
-                                    Export to CRIMS
+                                <v-list-item-title>
+                                   
                                 </v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
-                    </v-list>
+                    </v-list>-->
                 </v-col>
             </v-row>
 
@@ -40,7 +43,9 @@ export default {
         return {
             isExporting: false,
             options: ["Today", "This Week", "This Month", "3 Months Ago", "Custom Range"],
-            formData: {}
+            formData: {
+                date_range: []
+            }
         }
 
     },
