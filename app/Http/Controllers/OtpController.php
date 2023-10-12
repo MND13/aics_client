dd5e1af875fc95feeaf8b8991c6ead0c023e27d1
Mari Deleon <mari.deleon00@gmail.com>
2023-10-12 15:14:46 +0800

    fix otp expire

--- app/Http/Controllers/OtpController.php
+++ app/Http/Controllers/OtpController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\UserOtps;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Http;
 
 class OtpController extends Controller
 {
@@ -14,14 +16,32 @@ class OtpController extends Controller
             $otp = new UserOtps;
             $otp->user_id = Auth::user()->id;
             $otp->save();
-            return $otp;
+
+            if ($otp) 
+            {
+                $msg = "Maayong Adlaw! Kani na mensahe gikan sa DSWD Davao Region Office. Ang OTP mo ay ". $otp->otp ;
+                $response = Http::get('http://34.80.139.96/api/v2/SendSMS?ApiKey=LWtHZKzgbIh1sNQUPInRyqDFsj8W0K+8YCeSIdN08zA=&ClientId=3b3f49c9-b8e2-4558-9ed2-d618d7743fd5&SenderId=DSWD11AICS&Message=' . $msg . '&MobileNumbers=63' . substr(Auth::user()->mobile_number, 1));
+                $res = $response->collect();               
+                if($res["ErrorCode"] == 0)
+                {
+                    return [
+                        "success" => "OTP Sent to " . Auth::user()->mobile_number . ". OTP will expire in 30 min",
+                        "sms_response" =>  $res
+                    ];
+                }else
+                {
+                    return response()->json(["message" =>$res["ErrorDescription"] ], 422);
+                }
+            }
+
+           
         }
     }
 
     public function verify(Request $request)
     {
         $request->validate([
-          'otp' => 'required'
+            'otp' => 'required'
         ]);
 
         /* Validation Logic */
@@ -30,29 +50,28 @@ class OtpController extends Controller
 
 
         if (!$userOtp) {
-
-            return ["errors" => 'Your OTP is not correct'];
+            return response()->json(["message" => 'Your OTP is not correct'], 422);
         } else if ($userOtp && now()->isAfter($userOtp->expire_at)) {
-
-            return ["errors" => 'Your OTP has been expired'];
+            return response()->json(["message" => 'This OTP has expired'], 422);
         }
 
 
-        $user = User::whereId($request->user_id)->first();
+        $user = User::whereId(Auth::user()->id)->first();
 
 
 
         if ($user) {
-            $userOtp->update([
-                'expire_at' => now()
-            ]);
+            $userOtp->expire_at = now();
+            $userOtp->save();
+            
 
             $user->mobile_verified = 1;
             $user->save();
 
             //Auth::login($user);
+           
 
-            return ["success"=> "OTP Verified"];
+            return ["success" => "OTP Verified"];
         }
     }
 }
--- resources/js/Components/OtpGen.vue
+++ resources/js/Components/OtpGen.vue
@@ -2,7 +2,7 @@
     <v-container>
         <v-row>
 
-
+            {{ user }}
             <v-col cols="9" class="ma-auto">
 
                 <v-card flat tile outlined class="p-auto text-center">
@@ -11,31 +11,33 @@
                     <v-spacer></v-spacer>
                     <v-card-title class="justify-center">HI, {{ user.full_name }}! <br>
                         Please Verify Your Account <br></v-card-title>
-                    <v-card-subtitle>We are sending a OTP to validate your mobile number: {{ user.mobile_number }}
+                    <v-card-subtitle>We sent a OTP to validate your mobile number: {{ user.mobile_number }}
                     </v-card-subtitle>
                     <v-spacer></v-spacer>
                     <v-card-text class="justify-center ">
                         <v-row>
                             <v-col>
-                                <v-form>
+                                <v-form ref="form">
                                     <div class="ma-auto" style="max-width: 300px">
-                                        <v-otp-input v-model="formData.otp" length="6"></v-otp-input>
-                                        <v-btn color="primary" class="mb-3" @click="submitForm" :loading="loading"
-                                            :disabled="submit">
+                                        <v-otp-input v-model="formData.otp" length="6"
+                                            :rules="[(v) => !!v && v.length === 6 || 'its required']"
+                                            :error-messages="formErrors"></v-otp-input>
+                                        <v-btn color="primary" class="mb-3" @click="submitForm" :disabled="submit">
                                             Submit
                                         </v-btn>
                                     </div>
 
                                 </v-form>
 
-
+                                <v-alert type="success" v-if="success_message">{{ success_message }}</v-alert>
+                                <v-alert type="error" v-if="formErrors">{{ formErrors }}</v-alert>
 
 
                             </v-col></v-row>
 
 
                     </v-card-text>
-                    <v-card-actions class="justify-center">
+                    <v-card-actions class="justify-center mb-5">
                         Didn't receive a code?
                         <v-btn :loading="loading" class="ma-1" color="error" plain @click="otp_gen">
                             Resend
@@ -58,10 +60,11 @@ export default {
     data() {
         return {
             submit: false,
-            formData : {},
+            formData: {},
             loading: false,
             submit: false,
-            errors: [],
+            formErrors: "",
+            success_message: "",
         }
     },
     methods:
@@ -69,19 +72,35 @@ export default {
         submitForm() {
             this.submit = true;
             axios.post(route("api.otp.verify"), this.formData).then(response => {
-                console.log(response.data);
-                
-            }).catch(error => console.log(error));
-
+                console.log(response.data.success);
+                this.submit = false;
+                if (response.data.success) {
+                    this.formErrors = "";
+                    this.success_message = response.data.success;
+                    location.reload();
+                }
+            }).catch(e => {
+                this.formErrors = e.response.data.message
+            });
             this.submit = false;
-            
+
         },
         otp_gen() {
             this.loading = true;
-            axios.post(route("api.otp.generate")).then(repsonse => {
+            axios.post(route("api.otp.generate")).then(response => {
                 console.log(response);
                 this.loading = false;
-            }).catch(err => console.log(err))
+
+                if (response.data.success) {
+                    this.success_message = response.data.success;
+                    this.formErrors = "";
+                }
+
+
+                this.$refs.form.reset()
+            }).catch(e => {
+                this.formErrors = e.response.data.message
+            });
         }
     }
 }
