02d2c4c7bbc8feaf3ad86458d52725437076ff1e
Mari Nela C. De Leon <mcdeleon@ENTDSWD.LOCAL>
2024-03-26 16:30:08 +0800

    captcha in reset email

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +28,24 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:12|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/',
+            'g-recaptcha-response' => 'required|captcha',    
+        ];
+    }
+
+    public function validationErrorMessages()
+    {
+        return [           
+            'password.min' => 'The password must be at least 12 characters.',
+            'password.regex' => 'The password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.',
+            'g-recaptcha-response.required' => 'The reCAPTCHA field is required.',
+            'g-recaptcha-response.captcha' => 'The reCAPTCHA validation failed, please try again.',
+        ];
+    }
 }
